498435df201ede418c8bad114cdffe8554a36f64
redraw <agustinbv@gmail.com>
2018-12-23 14:15:44 -0300

    missing location command

--- telegram/run.js
+++ telegram/run.js
@@ -9,14 +9,18 @@ const session = require('telegraf/session')
 const bot = new Telegraf(process.env.TELEGRAM_TOKEN)
 
 bot.use(session())
-bot.use(requestLocation())
+bot.use(location())
 
 bot.start(ctx => {
   ctx.replyWithMarkdown('Buenos dias ✌')
 })
 
+bot.command('ubicacion', ctx => {
+  return requestLocation(ctx)
+})
+
 bot.on('text', ctx => {
-  ctx.reply('Espere un momentos')
+  ctx.replyWithChatAction('typing')
   const {latitude, longitude} = ctx.session.location
 
   api.buscar(ctx.message.text, latitude, longitude).then(data => {
@@ -39,11 +43,12 @@ bot.on('text', ctx => {
 bot.on('location', ctx => {
   console.log('[location]', ctx.message)
   ctx.session.location = ctx.message.location
-  ctx.reply('Dígame un producto')
+  ctx.reply('Dígame un producto o envía una foto con un codigo de barras🔎')
 })
 
 bot.on('photo', ctx => {
   ctx.reply('Espere un momentos')
+  ctx.replyWithChatAction('typing')
   console.log('[photo]', ctx.message)
   const photo = ctx.message.photo.pop()
   ctx.telegram.getFileLink(photo.file_id).then(url => {
@@ -52,10 +57,10 @@ bot.on('photo', ctx => {
       api.producto(code, latitude, longitude).then(data => {
         const p = data.producto
         const permalink = api.permalink(p.id, latitude, longitude)
-        ctx.replyWithMarkdown(`*${p.nombre}* [img](${api.img(p.id)}) | [link](${permalink})`)
+        ctx.replyWithMarkdown(`*${p.nombre}*\n[img](${api.img(p.id)}) | [link](${permalink})`)
         let text = data.sucursales.map(s => {
           const mapLink = `https://google.com/maps?q=${s.lat},${s.lng}`
-          return `[${s.banderaDescripcion}](${mapLink}) (${s.distanciaDescripcion}) $${s.preciosProducto.precioLista}`
+          return `📍 [${s.banderaDescripcion}](${mapLink}) (${s.distanciaDescripcion}) $${s.preciosProducto.precioLista}`
         }).join("\n")
         ctx.replyWithMarkdown(text, {
           disable_web_page_preview: true
@@ -67,20 +72,28 @@ bot.on('photo', ctx => {
   })
 })
 
-function requestLocation() {
+function location() {
   return (ctx, next) => {
     if (ctx.message.text == '/start' || ctx.session.location || ctx.message.location) {
       return next(ctx)
     } else {
-      return ctx.reply('Necesitos su ubicación para buscar precios cercas suyo',
-        Extra.markup(markup => {
-          return markup.oneTime().resize()
-            .keyboard([markup.locationRequestButton('Enviar ubicación')])
-          }
-        )
-      )
+      return requestLocation(ctx)
     }
   }
 }
+
+function requestLocation(ctx) {
+  return ctx.reply('Necesitaré su ubicación para buscar precios cercas suyo',
+    Extra.markup(markup => {
+      return markup.oneTime().resize()
+        .keyboard([markup.locationRequestButton('Enviar ubicación')])
+      }
+    )
+  )
+}
+
+// zeit now hack
+const server = require('http').createServer((req, res) => res.end('ok'))
+server.listen(8888)
 
 bot.startPolling()
